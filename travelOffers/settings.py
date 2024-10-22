0c5a45a73616964050a7c6f58b377aff83d9efc2
Ibon <castroibon216@gmail.com>
2024-10-22 13:23:56 +0200

    Crear aplicacion definitivo

--- travelOffers/settings.py
+++ travelOffers/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ofertas',
-    'paises',
-    'categorias',
+    'travelOffersApp',
 ]
 
 MIDDLEWARE = [

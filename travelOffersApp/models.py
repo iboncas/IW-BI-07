d85079ba1c33f21a4a2a665546e7c1fd208a05c5
David <davidprgt@gmail.com>
2024-10-28 13:04:49 +0100

    Modelos

--- travelOffersApp/models.py
+++ travelOffersApp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Pais(models.Model):
+    nombre = models.CharField(primary_key=True, max_length=100)
+    descripcion = models.TextField()
+    continente = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Categoria(models.Model):
+    nombre = models.CharField(primary_key=True, max_length=100)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+class Oferta(models.Model):
+    id_oferta = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE, related_name='ofertas')
+    categorias = models.ManyToManyField(Categoria, related_name='ofertas')
+    disponible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
